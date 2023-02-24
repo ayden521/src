5749a5322378054b679fcd39b3cd8c28a8551bee
ayden521 <126210816+ayden521@users.noreply.github.com>
2023-02-23 18:58:04 -0600

    adding updates

--- trader_interacive2.py
+++ trader_interacive2.py
@@ -5,16 +5,23 @@ db = mysql.connector.connect(
 )
 
 cur = db.cursor()
+cur.execute("SELECT MAX(price) AS Max Price FROM activities;") 
+max_result = cur.fetchall() 
+#for i in max_result:
+#    maximum = float(i[0])
+#    print(maximum)
 
-cur.execute("SELECT MAX(price) AS Max Price FROM activities;")
-  
-result = cur.fetchall()
-  
-for i in result:
-    maximum = float(i[0])
-    print(maximum)
+cur2 = db.cursor()
+cur2.execute("SELECT AVG(price) AS Average Price FROM activities;")
+ave_result = cur2.fetchall
 
-cur.execute("SELECT AVG(price) AS Average Price from activities;")
-  
+cur3 = db.cursor()
+cur3.execute("SELECT SUM(participants) as Total Participants FROM activities;" )
+sum_result = cur3.fetchall
+
+cur4 = db.cursor()
+cur4.execute("SELECT type as Type from FROM activities;")
+type_result = cur4.fetchall
+
 # Close database connection
 db.close()
--- trader_interactive.py
+++ trader_interactive.py
@@ -1,6 +1,7 @@
 # pull data from the API call
 import requests
 import json
+import mysql
 api_resopnse = requests.get('http://www.boredapi.com/api/activity').text
 response_info = json.loads(api_resopnse)
 print (response_info)
@@ -12,64 +13,71 @@ for key in response_info:
     inserts = ("INSERT INTO activities ({}) VALUES '{}';".format(key, value))
     print (inserts)
 
+
+
 # Serializing json
-json_object = json.dumps(response_info, indent=7)
-print (json_object)
+#json_object = json.dumps(response_info, indent=7)
+#print (json_object)
  
 # Writing to sample.json
-with open("sample.json", "w") as outfile:
-    outfile.write(json_object)
+#with open("sample.json", "w") as outfile:
+#    outfile.write(json_object)
 
-TABLE_NAME = "activities"
+#TABLE_NAME = "activities"
 
-sqlstatement = ''
-with open ('sample.json','r') as f:
-    jsondata = json.loads(f.read())
-    print(jsondata)
+#sqlstatement = ''
+#with open ('sample.json','r') as f:
+#    jsondata = json.loads(f.read())
+#    print(jsondata)
 
-for json in json_object:
-    keylist = "("
-    valuelist = "("
-    firstPair = True
-    for key, value in json_object:
-        if not firstPair:
-            keylist += ", "
-            valuelist += ", "
-        firstPair = False
-        keylist += key
-        if type(value) in (str, unicode):
-            valuelist += "'" + value + "'"
-        else:
-            valuelist += str(value)
-    keylist += ")"
-    valuelist += ")"
+#for json in json_object:
+#    keylist = "("
+#    valuelist = "("
+#    firstPair = True
+#    for key, value in json.items():
+#        if not firstPair:
+#            keylist += ", "
+#            valuelist += ", "
+#        firstPair = False
+#        keylist += key
+#        if type(value) in (str, unicode):
+#            valuelist += "'" + value + "'"
+#        else:
+#            valuelist += str(value)
+#    keylist += ")"
+#    valuelist += ")"
 
-    sqlstatement += "INSERT INTO " + TABLE_NAME + " " + keylist + " VALUES " + valuelist + "\n"
+#    sqlstatement += "INSERT INTO " + TABLE_NAME + " " + keylist + " VALUES " + valuelist + "\n"
 
-print(sqlstatement)
+#print(sqlstatement)
 # now how to get these insert statemetns inserted into mysql
-#import mysql.connector
+import mysql.connector
 
 #establishing the connection (this is a fake connection)
-#conn = mysql.connector.connect(
-#   user='root', password='password', host='localhost', database='mydb'
-#)
+connection = mysql.connector.connect(
+   user='root', password='password', host='localhost', database='mydb'
+)
 #Creating a cursor object using the cursor() method
-#cursor = conn.cursor()
+cursor = connection.cursor()
 
 #Creating table as per requirement
-#sql ='''CREATE TABLE `activities` (
-#`activity` varchar(100) DEFAULT NULL,
-#`type` varchar(100) DEFAULT NULL,
-#`participants` INTEGER,
-#`price` INTEGER,
-#`link` varchar(100) DEFAULT NULL,
-#`key` varchar(100) DEFAULT NULL,
-#`accessibility` INTEGER,
-#`create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-#);'''
-#cursor.execute(sql)
-#conn.commit()
+sql ='''CREATE TABLE `activities` (
+`activity` varchar(100) DEFAULT NULL,
+`type` varchar(100) DEFAULT NULL,
+`participants` INTEGER,
+`price` INTEGER,
+`link` varchar(100) DEFAULT NULL,
+`key` varchar(100) DEFAULT NULL,
+`accessibility` INTEGER,
+`create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+);'''
+cursor.execute(sql)
+connection.commit()
+
+# add the inserts from the API call
+# I don't think I've done this right...
+cursor.execute(inserts)
+connection.commit()
 
 #Closing the connection
-#conn.close()
+connection.close()
